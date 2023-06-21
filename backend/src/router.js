55c6b3225bf02b3c3758394021bf7251bcc34c17
israelfelipe9 <israelfelipe9@gmail.com>
2023-06-21 14:24:07 -0300

    All CRUD operations finished

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const tasksController = require("./controllers/taskController");
 const router = express.Router();
+const tasksMiddleware = require("./middlewares/tasksMiddleware");
 
 router.get("/tasks", tasksController.getAll);
+router.post(
+  "/tasks",
+  tasksMiddleware.validateFieldTitle,
+  tasksController.createTask
+);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put(
+  "/tasks/:id",
+  tasksMiddleware.validateFieldTitle,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
 
 module.exports = router;
